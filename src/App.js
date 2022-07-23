e2edc9ab3e493ade6c580a5ace0253b7df9ed8e2
David Patnode <draknor@gmail.com>
2022-07-23 08:54:29 -0500

    added cookie load function and alert/snackbar UI elements

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import WeeklyPlan from "./components/WeeklyPlan";
 import Header from "./components/Header";
 import {getTimeslots} from "./components/Timeslot";
 import {useCookies} from "react-cookie";
+import {Snackbar} from "@mui/material";
+import MuiAlert from "@mui/material/Alert";
 
 // This will eventually be replaced with a function/component to retrieve the tags from a user/data store
 const ResetTagArray = () => {
@@ -39,11 +41,24 @@ const App = () => {
   const [tags, setTags] = React.useState(ResetTagArray());
   const [planArray, setPlanArray] = React.useState(ResetPlanArray());
   const [cookies, setCookie, removeCookie] = useCookies(['weeklyPlan'])
+  const [alert, setAlert] = React.useState({open: false});
+
+  const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} {...props} />;
+  });
+
+  const handleAlertClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setAlert({open: false});
+  }
 
   const handleSaveClick = () => {
     // console.log("handleSaveClick - TBD");
     setCookie('tags',tags);
     setCookie('plan',planArray);
+    setAlert({open: true, severity: 'success', msg:'Saved!'})
   }
 
   const handleClearClick = () => {
@@ -51,13 +66,23 @@ const App = () => {
     setPlanArray(ResetPlanArray()); //TODO this doesn't clear the TagMenu selected items - how to do that?!
     removeCookie('tags');
     removeCookie('plan');
+    setAlert({open: true, severity: 'warning', msg:'Deleted!'})
+  }
+
+  const handleLoadClick = () => {
+    if (cookies.tags) { setTags(cookies.tags); }
+    if (cookies.plan) { setPlanArray(cookies.plan); }
+    setAlert({open: true, severity: 'success', msg:'Loaded!'})
   }
 
   return (
     <Container maxWidth={"lg"}>
-      <Header saveOnClick={handleSaveClick} clearOnClick={handleClearClick}/>
+      <Header saveOnClick={handleSaveClick} clearOnClick={handleClearClick} loadOnClick={handleLoadClick}/>
       <TagCollection tags={tags}/>
       <WeeklyPlan tags={tags} setTags={setTags} planArray={planArray} setPlanArray={setPlanArray} />
+      <Snackbar open={alert.open} autoHideDuration={3000} onClose={handleAlertClose}>
+        <Alert onClose={handleAlertClose} severity={alert.severity}>{alert.msg}</Alert>
+      </Snackbar>
       <Footer />
     </Container>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,21 @@
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import {Button} from "@mui/material";
-import {Save, Delete} from "@mui/icons-material";
+import {Save, Delete, Backup} from "@mui/icons-material";
 import Stack from "@mui/material/Stack";
 
 
 const Header = (props) => {
   const saveOnClick = props.saveOnClick;
   const clearOnClick = props.clearOnClick;
+  const loadOnClick = props.loadOnClick;
   return (
     <Box sx={{ my: 4 }}>
         <Stack spacing={5} direction={"row"}>
           <Typography variant="h2" gutterBottom>
             My168 Planner
           </Typography>
+          <Button variant={"contained"} color={"info"} startIcon={<Backup />} onClick={loadOnClick}>Load</Button>
           <Button variant={"contained"} color={"success"} startIcon={<Save />} onClick={saveOnClick}>Save</Button>
           <Button variant={"contained"} color={"error"} startIcon={<Delete />} onClick={clearOnClick}>Clear</Button>
         </Stack>
