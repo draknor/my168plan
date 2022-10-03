b48106ca3e2a21ac8171ec64d93692e84a6bbc28
David Patnode <draknor@gmail.com>
2022-10-02 19:13:50 -0500

    update default schedule

--- src/operations/reset.js
+++ src/operations/reset.js
@@ -4,23 +4,23 @@ const ResetTags = () => {
   return [
     { id: 1, name: "Sleep", cssClass: 'tagColor1', comment: "Sleep 8 hours per night" },
     { id: 2, name: "Eat", cssClass: 'tagColor6', comment: "Spend an hour prepping, cooking, cleaning per meal, 3 meals/day"},
-    { id: 3, name: "Family Time", cssClass: 'tagColor3', comment: "" },
+    { id: 3, name: "Family Time", cssClass: 'tagColor3', comment: "Spend time with my family / close friends" },
     { id: 4, name: "Work", cssClass: 'tagColor9', comment: "Work 8-5 every week day (with one-hour lunch break)"},
-    { id: 5, name: "Recreation", cssClass: 'tagColor5', comment: ""},
-    { id: 6, name: "Spirituality", cssClass: 'tagColor2', comment: ""},
+    { id: 5, name: "Recreation", cssClass: 'tagColor5', comment: "Spend time playing games, sports, reading, watching movies"},
+    { id: 6, name: "Spirituality", cssClass: 'tagColor2', comment: "Spend time praying, meditating, attending religious services"},
   ];
 }
 
 const DefaultPlan = () => {
   let plan = Array(7);
   // Setup a default plan
-  plan[0] = [1, 1, 1, 1, 1, 1, 1, 2, null, null, null, null, 2, null, null, null, null, 2, null, null, null, null, 1, 1 ];
-  plan[1] = [1, 1, 1, 1, 1, 1, 1, 2, 4, 4, 4, 4, 2, 4, 4, 4, 4, 2, 3, 3, 5, 5, 1, 1 ];
-  plan[2] = [1, 1, 1, 1, 1, 1, 1, 2, 4, 4, 4, 4, 2, 4, 4, 4, 4, 2, 3, 3, 5, 5, 1, 1 ];
-  plan[3] = [1, 1, 1, 1, 1, 1, 1, 2, 4, 4, 4, 4, 2, 4, 4, 4, 4, 2, 3, 3, 5, 5, 1, 1 ];
-  plan[4] = [1, 1, 1, 1, 1, 1, 1, 2, 4, 4, 4, 4, 2, 4, 4, 4, 4, 2, 3, 3, 5, 5, 1, 1 ];
-  plan[5] = [1, 1, 1, 1, 1, 1, 1, 2, 4, 4, 4, 4, 2, 4, 4, 4, 4, 2, 3, 3, 5, 5, 1, 1 ];
-  plan[6] = [1, 1, 1, 1, 1, 1, 1, 2, null, null, null, null, 2, null, null, null, null, 2, null, null, null, null, 1, 1 ];
+  plan[0] = [1, 1, 1, 1, 1, 1, 1, 2, null, null, null, null, 2, null, null, null, null, 2, null, null, null, null, null, 1 ];
+  plan[1] = [1, 1, 1, 1, 1, 1, 1, 2, 4, 4, 4, 4, 2, 4, 4, 4, 4, 2, 5, 5, 3, 3, 6, 1 ];
+  plan[2] = [1, 1, 1, 1, 1, 1, 1, 2, 4, 4, 4, 4, 2, 4, 4, 4, 4, 2, 5, 5, 3, 3, 6, 1 ];
+  plan[3] = [1, 1, 1, 1, 1, 1, 1, 2, 4, 4, 4, 4, 2, 4, 4, 4, 4, 2, 5, 5, 3, 3, 6, 1 ];
+  plan[4] = [1, 1, 1, 1, 1, 1, 1, 2, 4, 4, 4, 4, 2, 4, 4, 4, 4, 2, 5, 5, 3, 3, 6, 1 ];
+  plan[5] = [1, 1, 1, 1, 1, 1, 1, 2, 4, 4, 4, 4, 2, 4, 4, 4, 4, 2, 5, 5, 3, 3, 6, 1 ];
+  plan[6] = [1, 1, 1, 1, 1, 1, 1, 2, null, null, null, null, 2, null, null, null, null, 2, null, null, null, null, null, 1 ];
   return plan;
 }
 
